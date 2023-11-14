833740bec229599cfdb07809db118fb9fbef870a
souccar <85060233+souccar-github@users.noreply.github.com>
2023-11-14 15:18:24 +0200

    Create Entities #1

---
+++ src/Souccar.Core/Hcpc/Plans/Plan.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Abp.Domain.Entities;
+
+namespace Souccar.Hcpc.Plans
+{
+    public class Plan : FullAuditedAggregateRoot<int>, IMayHaveTenant
+    {
+        public int? TenantId { get; set; }
+    }
+}
