077fe296d3f63ec59a7124852926cda1ae577a65
Mohamad Tonbary <ttmohamad299@gmail.com>
2023-11-23 10:21:57 +0200

    #26

--- src/Souccar.Core/Hcpc/Materials/Services/IMaterialManager.cs
+++ src/Souccar.Core/Hcpc/Materials/Services/IMaterialManager.cs
@@ -4,5 +4,6 @@ namespace Souccar.Hcpc.Materials.Services
 {
     public interface IMaterialManager: ISouccarDomainService<Material,int>
     {
+       Material GetWithDetails(int id);
     }
 }
--- src/Souccar.Core/Hcpc/Materials/Services/MaterialManager.cs
+++ src/Souccar.Core/Hcpc/Materials/Services/MaterialManager.cs
@@ -1,13 +1,24 @@
 ﻿using Abp.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Souccar.Core.Services.Implements;
+using Souccar.Hcpc.Plans;
+using System.Linq;
 
 namespace Souccar.Hcpc.Materials.Services
 {
     public class MaterialManager: SouccarDomainService<Material, int>, IMaterialManager
     {
-        public MaterialManager(IRepository<Material> repository): base(repository)
+        private readonly IRepository<Material> _materialRepository;
+        public MaterialManager(IRepository<Material> repository, IRepository<Material> materialRepository) : base(repository)
         {
-            
+            _materialRepository = materialRepository;
+        }
+
+        public Material GetWithDetails(int id)
+        {
+            var material = _materialRepository.GetAllIncluding().Include(x=>x.Suppliers).ThenInclude(y=>y.Supplier).FirstOrDefault(x=>x.Id== id);
+
+            return material;
         }
     }
 }
