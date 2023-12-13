6912d9c9cfed2c179dc4dc96cf973abc83e60874
souccar <85060233+souccar-github@users.noreply.github.com>
2023-12-13 13:48:33 +0200

    354dsa54

---
+++ src/Souccar.EntityFrameworkCore/Migrations/20231213114711_Add_DailyProduction_Note.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Souccar.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_DailyProduction_Note : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "DailyProductionNotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenantId = table.Column<int>(type: "int", nullable: true),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DailyProductionId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyProductionNotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DailyProductionNotes_DailyProductions_DailyProductionId",
+                        column: x => x.DailyProductionId,
+                        principalTable: "DailyProductions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DailyProductionNotes_DailyProductionId",
+                table: "DailyProductionNotes",
+                column: "DailyProductionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyProductionNotes");
+
+        }
+    }
+}
