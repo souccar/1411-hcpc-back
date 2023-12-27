5a33182afaf6877acb01c6986dc5ee1347edbce6
Mohammad Alimam <mohammad.imam2016@gmail.com>
2023-12-27 11:33:01 +0200

    1

--- src/Souccar.Application/Hcpc/WarehousesApp/WarehouseMaterials/Dto/CreateWarehouseMaterialDto.cs
+++ src/Souccar.Application/Hcpc/WarehousesApp/WarehouseMaterials/Dto/CreateWarehouseMaterialDto.cs
@@ -25,7 +25,6 @@ namespace Souccar.Hcpc.WarehousesApp.WarehouseMaterials.Dto
         [Required(ErrorMessage = SouccarAppConstant.Required)]
         public int? UnitId { get; set; }
 
-        [Required(ErrorMessage = SouccarAppConstant.Required)]
         public int? UnitPriceId { get; set; }
 
         [Required(ErrorMessage = SouccarAppConstant.Required)]
