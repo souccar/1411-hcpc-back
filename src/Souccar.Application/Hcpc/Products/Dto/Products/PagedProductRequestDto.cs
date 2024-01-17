3b144980eacdacb8ab8400da16fcb78f5ef4f615
Raghad-alshaita <raghadalsh99@gmail.com>
2024-01-17 09:32:49 +0200

    gh

--- src/Souccar.Application/Hcpc/Products/Dto/Products/PagedProductRequestDto.cs
+++ src/Souccar.Application/Hcpc/Products/Dto/Products/PagedProductRequestDto.cs
@@ -1,10 +1,13 @@
 ﻿using Abp.Application.Services.Dto;
+using Souccar.Core.Filter;
 
 namespace Souccar.Hcpc.Products.Dto.Products
 {
-    public class PagedProductRequestDto : PagedResultRequestDto, ISortedResultRequest
+    public class PagedProductRequestDto : PagedResultRequestDto, ISortedResultRequest, IFilterResultRequest
     {
         public string Keyword { get; set; }
+        public FilterDto Filtering { get; set; }
         public string Sorting { get; set; }
+
     }
 }
