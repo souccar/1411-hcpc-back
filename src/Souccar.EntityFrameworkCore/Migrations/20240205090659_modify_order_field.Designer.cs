40a80a2dfc3843babd48fae911b2aa4e4e0e5f71
Mohamad Tonbary <ttmohamad299@gmail.com>
2024-02-05 11:24:31 +0200

    #107 Modify Order Field Name

--- src/Souccar.EntityFrameworkCore/Migrations/20240205090659_modify_order_field.Designer.cs
+++ src/Souccar.EntityFrameworkCore/Migrations/20240205090659_modify_order_field.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Souccar.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Souccar.EntityFrameworkCore;
 namespace Souccar.Migrations
 {
     [DbContext(typeof(SouccarDbContext))]
-    partial class SouccarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240205090659_modify_order_field")]
+    partial class modify_order_field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
