4bd4a5f062e9d93a38a8e0960771a369b7eaec13
Mohamad Tonbary <ttmohamad299@gmail.com>
2024-02-19 11:46:25 +0200

    #109

--- src/Souccar.Application/Hcpc/WarehousesApp/OutputRequests/Dto/PagedOutputRequestDto.cs
+++ src/Souccar.Application/Hcpc/WarehousesApp/OutputRequests/Dto/PagedOutputRequestDto.cs
@@ -1,12 +1,14 @@
 ﻿using Abp.Application.Services.Dto;
+using Souccar.Core.Filter;
 using Souccar.Core.Includes;
 
 namespace Souccar.Hcpc.WarehousesApp.OutputRequests.Dto
 {
-    public class PagedOutputRequestDto : PagedResultRequestDto, ISortedResultRequest, IIncludeResultRequest
+    public class PagedOutputRequestDto : PagedResultRequestDto, ISortedResultRequest, IIncludeResultRequest, IFilterResultRequest
     {
         public string Keyword { get; set; }
         public string Sorting { get; set; }
         public string Including { get; set; }
+        public FilterDto Filtering { get; set; }
     }
 }
